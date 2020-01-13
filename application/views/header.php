0bf42fc122bad1e57339ab9b176c5de431e9eddf
= <im@fahimahmed.com>
2020-01-13 18:49:24 +0500

    template updated

--- application/views/header.php
+++ application/views/header.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title><?php echo SYSTEM_NAME; ?></title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta name="robots" content="noindex, nofollow" />
     <meta content="" name="keywords">
     <meta content="" name="description">
 
